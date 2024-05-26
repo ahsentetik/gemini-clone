e58de99196795130b64f0092596e9593988b06be
ahsentetik <110971409+ahsentetik@users.noreply.github.com>
2024-05-26 12:34:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react' // React'ı içe aktarır
+import Main from './component /Main/Main'
+import Sidebar from './component /Sidebar/Sidebar'
+
+function App() {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App  // App bileşeninin dışarı aktarmayı sağlar
